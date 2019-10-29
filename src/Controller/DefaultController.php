3927930505d320a6119de888a6d4dcd78c2f0a02
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-10-30 00:15:25 +0200

    Root route

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        return $this->json([
+            'ok' => true,
+            'error' => false,
+        ]);
+    }
+}
