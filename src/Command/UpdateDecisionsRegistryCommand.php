8879fd3e0774becb2e87de123c3fd193ab91d202
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-11-01 22:13:04 +0200

    Stub for update registry command

---
+++ src/Command/UpdateDecisionsRegistryCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class UpdateDecisionsRegistryCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'app:registry:update';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Updates decisions registry')
+            ->setHelp('Use this to update decisions registry')
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Hello, world!!');
+        $output->writeln('You are about to update decisions registry');
+        return 0;
+    }
+}
