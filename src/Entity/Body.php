599ef8cab07227c9499fbe02a35170448fdbcfdb
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-11-04 22:57:49 +0200

    Added entity for Dispute Category

--- src/Entity/Body.php
+++ src/Entity/Body.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,6 +27,16 @@ class Body
      * @ORM\Column(type="string", length=255)
      */
     private $ShortName;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Category", mappedBy="Body", orphanRemoval=true)
+     */
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -51,6 +63,37 @@ class Body
     public function setShortName(string $ShortName): self
     {
         $this->ShortName = $ShortName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+            $category->setBody($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->contains($category)) {
+            $this->categories->removeElement($category);
+            // set the owning side to null (unless already changed)
+            if ($category->getBody() === $this) {
+                $category->setBody(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Category.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ShortName;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Body", inversedBy="categories")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Body;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getShortName(): ?string
+    {
+        return $this->ShortName;
+    }
+
+    public function setShortName(string $ShortName): self
+    {
+        $this->ShortName = $ShortName;
+
+        return $this;
+    }
+
+    public function getBody(): ?Body
+    {
+        return $this->Body;
+    }
+
+    public function setBody(?Body $Body): self
+    {
+        $this->Body = $Body;
+
+        return $this;
+    }
+}
