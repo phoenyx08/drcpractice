34a4b3a0f3c6fab0e839c25bbae8f185dac22aa8
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-12-17 23:56:33 +0200

    Start reconstruction of FakeParser

--- src/Service/FakeParser.php
+++ src/Service/FakeParser.php
@@ -5,94 +5,69 @@ namespace App\Service;
 use PhoenyxStudio\Parser\AbstractParser;
 use PhoenyxStudio\Parser\ParseResult\IParseResult;
 use PhoenyxStudio\Parser\ParseResult\GeneralParseResult;
+use App\Entity\Decision;
+use App\Entity\Category;
+use DOMDocument;
+use DOMXPath;
 
 
 class FakeParser extends AbstractParser
 {
-    private function createObject()
+    /**
+     * @var xpath
+     *
+     * In further versions better to move to AbstractParser
+     */
+    protected $xpath;
+    protected $rootXpath = '/section';
+    private $yearNodesXpath = 'div[2]/div/div/div[@class="fi-document-table fi-table"]';
+
+    private function init()
     {
-        $a = new Class {
-            private $title;
-            private $heading;
-            private $listItems;
-            /**
-             * @return mixed
-             */
-            public function getTitle()
-            {
-                return $this->title;
-            }
-
-            /**
-             * @param mixed $title
-             */
-            public function setTitle($title)
-            {
-                $this->title = $title;
-            }
-
-            /**
-             * @return mixed
-             */
-            public function getHeading()
-            {
-                return $this->heading;
-            }
-
-            /**
-             * @param mixed $heading
-             */
-            public function setHeading($heading)
-            {
-                $this->heading = $heading;
-            }
-            public function addItem(string $text)
-            {
-                $this->listItems[] = $text;
-            }
-            public function getListItems()
-            {
-                return $this->listItems;
-            }
-        };
-        return $a;
+        $this->domDocument = new DOMDocument();
+        $this->domDocument->loadXML($this->source);
+        $this->xpath = new DOMXPath($this->domDocument);
     }
+
+    private function getFirstElementByXpath(string $xpath, DOMElement $element)
+    {
+        return $this->xpath
+            ->query($xpath, $element)
+            ->item(0);
+    }
+
+    private function getElementsByXpath(string $xpath, \DOMElement $element)
+    {
+        return $this->xpath
+            ->query($xpath, $element);
+    }
+
+    private function getRootElement()
+    {
+        return $this->xpath
+            ->query($this->rootXpath)
+            ->item(0);
+    }
+
     public function parse(string $source) : IParseResult
     {
         $parseResult = new GeneralParseResult();
 
-        //$doc = new ParsedDocument();
-        //
-        $doc = $this->createObject();
-
         $this->source = $source;
+        $this->init();
 
-        //$html = $this->getHtmlElement();
+        $xpath = $this->xpath;
 
-        //$xpath = new \DOMXPath($this->domDocument);
-        $domDoc = new \DOMDocument();
-        $domDoc->loadXML($source);
-        $xpath = new \DOMXPath($domDoc);
+        $root = $this->getRootElement();
 
-        // get contents of title ta
-        //$query = 'head/title';
-        $query = '/section';
-        //$title = $xpath->query($query, $html)->item(0)->textContent;
-        $title = $xpath->query($query);
-        $root = $title->item(0);
-        $doc->setTitle($title);
-
-        // get year divs
-        //$query = 'div/div/div[@class="fi-document-table fi-table"]';
-        $query = 'div[2]/div/div/div[@class="fi-document-table fi-table"]';
-        $list = $xpath->query($query, $root);
+        $list = $this->getElementsByXpath($this->yearNodesXpath, $root);
         $yearNodes = [];
-        //$doc->setHeading($h1);
+
         foreach($list as $item) {
             $yearNodes[] = $item;
         }
         $months = [];
-        foreach($yearNodes as $yearNode){
+        foreach($yearNodes as $yearNode) {
             // get year title
             $query = 'div/div/div/span';
             $list = $xpath->query($query, $yearNode);
@@ -126,23 +101,7 @@ class FakeParser extends AbstractParser
         }
         print_r($months);
 
-
-
-
-
-        // get contents of h1 in content div
-        $query = 'body/div[@class="content"]/h1';
-        //$h1 = $xpath->query($query, $html)->item(0)->textContent;
-        //$doc->setHeading($h1);
-
-        // get li elements of list in the body
-        $query = 'body/div[@class="content"]/ul/li';
-        //$lis = $xpath->query($query, $html);
-        //foreach ($lis as $li) {
-        //    $doc->addItem($li->textContent);
-        //}
-
-        $parseResult->setObject($doc);
+        //$parseResult->setObject($doc);
 
         return $parseResult;
     }
