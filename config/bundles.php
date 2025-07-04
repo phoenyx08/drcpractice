6b9573df84077e61efa9e6dd03d63eef30553b24
Yuriy Serdyuk <yurko_ne@ukr.net>
2025-07-04 22:36:19 +0100

    Basic Frontend

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,61 @@
 
 namespace App\Controller;
 
+use App\Repository\BodyRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\DecisionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/", name="default")
-     */
-    public function index()
+    #[Route('/', name: 'homepage')]
+    public function index(BodyRepository $bodyRepository, CategoryRepository $categoryRepository): Response
     {
-        return $this->json([
-            'ok' => true,
-            'error' => false,
+        $bodies = $bodyRepository->findAll();
+        $allCategories = $categoryRepository->findAll();
+        
+        return $this->render('homepage.html.twig', [
+            'bodies' => $bodies,
+            'allCategories' => $allCategories,
+        ]);
+    }
+    
+    #[Route('/body/{id}', name: 'body')]
+    public function body(int $id, BodyRepository $bodyRepository): Response
+    {
+        $body = $bodyRepository->find($id);
+        
+        if (!$body) {
+            throw $this->createNotFoundException('Body not found');
+        }
+        
+        $bodies = $bodyRepository->findAll();
+        
+        return $this->render('homepage.html.twig', [
+            'bodies' => $bodies,
+            'activeBody' => $body,
+        ]);
+    }
+    
+    #[Route('/category/{id}', name: 'category')]
+    public function category(int $id, CategoryRepository $categoryRepository, BodyRepository $bodyRepository, DecisionRepository $decisionRepository): Response
+    {
+        $category = $categoryRepository->find($id);
+        
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+        
+        $bodies = $bodyRepository->findAll();
+        $decisions = $decisionRepository->findBy(['category' => $category], ['year' => 'DESC', 'month' => 'DESC']);
+        
+        return $this->render('homepage.html.twig', [
+            'bodies' => $bodies,
+            'activeBody' => $category->getBody(),
+            'category' => $category,
+            'decisions' => $decisions,
         ]);
     }
 }
